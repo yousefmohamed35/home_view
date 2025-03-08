4f1730d2b5e073d1e0dea1f1861a81d5006ec50b
yousefmohamed35 <yousefmo2021jo@gmail.com>
2025-03-08 16:17:34 +0200

    add padding to custom container app bar

--- lib/home/presentation/view/widgets/custom_container_app_bar.dart
+++ lib/home/presentation/view/widgets/custom_container_app_bar.dart
@@ -8,19 +8,22 @@ class CustomContainerAppBar extends StatelessWidget {
     return Container(
       color: Color(0xff2B5B8D),
       height: 153,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text(
-            'Hello, Karim',
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.w600,
-              color: Color(0xffF9F6EE),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              'Hello, Karim',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w600,
+                color: Color(0xffF9F6EE),
+              ),
             ),
-          ),
-          Icon(Icons.notifications_outlined, color: Color(0xffF9F6EE)),
-        ],
+            Icon(Icons.notifications_outlined, color: Color(0xffF9F6EE)),
+          ],
+        ),
       ),
     );
   }
