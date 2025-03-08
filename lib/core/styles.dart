4a626d73959d12ef02e5ee3df6d548622abaab3b
yousefmohamed35 <yousefmo2021jo@gmail.com>
2025-03-08 16:31:34 +0200

    create styles

--- lib/core/styles.dart
+++ lib/core/styles.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'app_colors.dart';
+
 
 abstract class AppStyles {
   //10
@@ -11,103 +13,87 @@ abstract class AppStyles {
   static const medium10 = TextStyle(
     fontSize: 10,
     fontWeight: FontWeight.w500,
-    fontFamily: Constants.fontFamily,
     color: AppColors.grey400,
   );
   static const bold10 = TextStyle(
     fontSize: 10,
     fontWeight: FontWeight.bold,
-    fontFamily: Constants.fontFamily,
     color: AppColors.grey400,
   );
   //12
   static const regular12 = TextStyle(
     fontSize: 12,
     fontWeight: FontWeight.normal,
-    fontFamily: Constants.fontFamily,
     color: AppColors.grey400,
   );
   static const medium12 = TextStyle(
     fontSize: 12,
     fontWeight: FontWeight.w500,
-    fontFamily: Constants.fontFamily,
     color: AppColors.grey400,
   );
   static const bold12 = TextStyle(
     fontSize: 12,
     fontWeight: FontWeight.bold,
-    fontFamily: Constants.fontFamily,
     color: AppColors.grey400,
   );
   //14
   static const regular14 = TextStyle(
     fontSize: 14,
     fontWeight: FontWeight.normal,
-    fontFamily: Constants.fontFamily,
     color: AppColors.grey500,
   );
   static const medium14 = TextStyle(
     fontSize: 14,
     fontWeight: FontWeight.w500,
-    fontFamily: Constants.fontFamily,
     color: AppColors.grey500,
   );
   static const bold14 = TextStyle(
     fontSize: 14,
     fontWeight: FontWeight.bold,
-    fontFamily: Constants.fontFamily,
     color: AppColors.grey500,
   );
   //16
   static const regular16 = TextStyle(
     fontSize: 16,
     fontWeight: FontWeight.normal,
-    fontFamily: Constants.fontFamily,
     color: AppColors.black,
   );
   static const medium16 = TextStyle(
     fontSize: 16,
     fontWeight: FontWeight.w500,
-    fontFamily: Constants.fontFamily,
     color: AppColors.black,
   );
   static const bold16 = TextStyle(
     fontSize: 16,
     fontWeight: FontWeight.bold,
-    fontFamily: Constants.fontFamily,
     color: AppColors.black,
   );
   //18
   static const medium18 = TextStyle(
     fontSize: 18,
     fontWeight: FontWeight.w500,
-    fontFamily: Constants.fontFamily,
     color: AppColors.black,
   );
   static const bold18 = TextStyle(
     fontSize: 18,
     fontWeight: FontWeight.bold,
-    fontFamily: Constants.fontFamily,
     color: AppColors.black,
   );
   //20
   static const medium20 = TextStyle(
     fontSize: 20,
     fontWeight: FontWeight.w500,
-    fontFamily: Constants.fontFamily,
     color: AppColors.black,
   );
   static const bold20 = TextStyle(
     fontSize: 20,
     fontWeight: FontWeight.bold,
-    fontFamily: Constants.fontFamily,
     color: AppColors.black,
   );
   //24
   static const bold24 = TextStyle(
     fontSize: 24,
     fontWeight: FontWeight.bold,
-    fontFamily: Constants.fontFamily,
     color: AppColors.black,
   );
 }
