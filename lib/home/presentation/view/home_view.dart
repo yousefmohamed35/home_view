dc07023166cc0a4888829c2824e2209cabc1931d
yousefmohamed35 <yousefmo2021jo@gmail.com>
2025-03-13 13:54:59 +0200

    create home view body

--- lib/home/presentation/view/home_view.dart
+++ lib/home/presentation/view/home_view.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:homeview/home/presentation/view/widgets/company_review_item_list_view.dart';
-import 'widgets/company_review_title.dart';
-import 'widgets/custom_container_app_bar.dart';
 import 'widgets/custom_floating_action_button.dart';
-import 'widgets/home_view_item.dart';
+import 'widgets/home_view_body.dart';
+
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -13,20 +11,8 @@ class HomeView extends StatelessWidget {
     return Scaffold(
       floatingActionButton: CustomFloatingActionButton(),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            CustomContainerAppBar(),
-            SizedBox(height: 60),
-            HomeViewItem(),
-            SizedBox(height: 20),
-            CompanyReviewTitle(),
-            SizedBox(height: 20),
-            CompanyReviewItemListView(),
-            SizedBox(height: 20),
-          ],
-        ),
-      ),
+      body: HomeViewBody(),
     );
   }
 }
+
