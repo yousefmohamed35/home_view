02a6669e1d9664dc9c311fe1558d5f9afa4c7e30
yousefmohamed35 <yousefmo2021jo@gmail.com>
2025-03-22 15:19:58 +0200

    update search item

--- lib/home/presentation/view/widgets/search_items.dart
+++ lib/home/presentation/view/widgets/search_items.dart
@@ -2,37 +2,50 @@ import 'package:flutter/material.dart';
 import 'package:homeview/core/app_colors.dart';
 import 'package:homeview/home/presentation/view/widgets/search_job_widget.dart';
 
+import '../../../../search/presentation/view/search_view.dart';
+
 class SearchItems extends StatelessWidget {
   const SearchItems({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.all(0),
-      color: Color(0xffE9E9E9),
-      
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(20),
-        side: BorderSide(color: AppColors.borderColor, width: 0.5),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-        child: Column(
-          children: [
-            SearchJobWidget(
-              text: 'Search Job',
-              prefixIcon: Icons.search_outlined,
-              suffixIcon: Icons.tune,
-            ),
-            Divider(color: Color(0xffCFCFCF), height: 10),
-            SearchJobWidget(
-              text: 'Type your location',
-              prefixIcon: Icons.location_on_outlined,
-            ),
-          ],
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return SearchView();
+            },
+          ),
+        );
+      },
+      child: Card(
+        margin: EdgeInsets.all(0),
+        color: Color(0xffE9E9E9),
+
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+          side: BorderSide(color: AppColors.borderColor, width: 0.5),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+          child: Column(
+            children: [
+              SearchJobWidget(
+                text: 'Search Job',
+                prefixIcon: Icons.search_outlined,
+                suffixIcon: Icons.tune,
+              ),
+              Divider(color: Color(0xffCFCFCF), height: 10),
+              SearchJobWidget(
+                text: 'Type your location',
+                prefixIcon: Icons.location_on_outlined,
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
-
