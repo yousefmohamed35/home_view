5934aff7256ff192644da79fb2dd134c51b01ef3
yousefmohamed35 <yousefmo2021jo@gmail.com>
2025-03-22 14:41:09 +0200

    update home view item

--- lib/home/presentation/view/widgets/custom_button.dart
+++ lib/home/presentation/view/widgets/custom_button.dart
@@ -4,34 +4,38 @@ import 'package:homeview/core/app_colors.dart';
 import 'package:homeview/core/styles.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key, required this.text, this.isIcon = false});
+  const CustomButton({super.key, required this.text, this.isIcon = false, this.onTap});
   final String text;
   final bool isIcon;
+  final void Function()? onTap;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      width: 145,
-      height: 40,
-      decoration: BoxDecoration(
-        color: isIcon ? Colors.transparent : AppColors.blue,
-        borderRadius: BorderRadius.circular(20),
-        border: Border.all(color: AppColors.blue),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          isIcon ? Icon(Icons.pages, color: AppColors.blue) : SizedBox(),
-          SizedBox(width: 8),
-          Text(
-            text,
-            style: GoogleFonts.lato(
-              textStyle: AppStyles.medium20.copyWith(
-                color: isIcon ? AppColors.blue : AppColors.main,
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        alignment: Alignment.center,
+        width: 145,
+        height: 40,
+        decoration: BoxDecoration(
+          color: isIcon ? Colors.transparent : AppColors.blue,
+          borderRadius: BorderRadius.circular(20),
+          border: Border.all(color: AppColors.blue),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            isIcon ? Icon(Icons.pages, color: AppColors.blue) : SizedBox(),
+            SizedBox(width: 8),
+            Text(
+              text,
+              style: GoogleFonts.lato(
+                textStyle: AppStyles.medium20.copyWith(
+                  color: isIcon ? AppColors.blue : AppColors.main,
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
--- lib/home/presentation/view/widgets/home_view_item.dart
+++ lib/home/presentation/view/widgets/home_view_item.dart
@@ -13,58 +13,53 @@ class HomeViewItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: InkWell(
-        onTap: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) {
-                return JobDescriptionView();
-              },
-            ),
-          );
-        },
-        child: Container(
-          decoration: BoxDecoration(
-            color: AppColors.grey,
-            borderRadius: BorderRadius.circular(8),
-            border: Border.all(width: 0.5, color: AppColors.borderColor),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(8),
-            child: Column(
-              children: [
-                HomeViewItemTop(),
-                Divider(
-                  color: Color(0xff95948F),
-                  indent: 10,
-                  endIndent: 10,
-                  height: 20,
-                ),
-                InformationItem(
-                  text: 'cairo',
-                  icon: Icons.location_on_outlined,
-                ),
-                SizedBox(height: 10),
-                InformationItem(text: 'Full time', icon: Icons.schedule),
-                SizedBox(height: 10),
-                InformationItem(
-                  text: '6000 EGP/Month',
-                  icon: Icons.money_sharp,
-                ),
-                SizedBox(height: 10),
-                CustomRating(),
-                SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    CustomButton(text: 'Save', isIcon: true),
-                    CustomButton(text: 'View'),
-                  ],
-                ),
-                SizedBox(height: 30),
-              ],
-            ),
+      child: Container(
+        decoration: BoxDecoration(
+          color: AppColors.grey,
+          borderRadius: BorderRadius.circular(8),
+          border: Border.all(width: 0.5, color: AppColors.borderColor),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: Column(
+            children: [
+              HomeViewItemTop(),
+              Divider(
+                color: Color(0xff95948F),
+                indent: 10,
+                endIndent: 10,
+                height: 20,
+              ),
+              InformationItem(text: 'cairo', icon: Icons.location_on_outlined),
+              SizedBox(height: 10),
+              InformationItem(text: 'Full time', icon: Icons.schedule),
+              SizedBox(height: 10),
+              InformationItem(text: '6000 EGP/Month', icon: Icons.money_sharp),
+              SizedBox(height: 10),
+              CustomRating(),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  CustomButton(
+                    text: 'Save',
+                    isIcon: true,
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return JobDescriptionView();
+                          },
+                        ),
+                      );
+                    },
+                  ),
+                  CustomButton(text: 'View'),
+                ],
+              ),
+              SizedBox(height: 30),
+            ],
           ),
         ),
       ),
