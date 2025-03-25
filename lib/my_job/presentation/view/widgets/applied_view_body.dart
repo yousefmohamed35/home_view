86057a7fb3f63b7256a6704148c330fd94b155e4
yousefmohamed35 <yousefmo2021jo@gmail.com>
2025-03-25 03:06:12 +0200

    finish applied view

--- lib/my_job/presentation/view/widgets/applied_view_body.dart
+++ lib/my_job/presentation/view/widgets/applied_view_body.dart
@@ -3,16 +3,21 @@ import 'package:flutter/material.dart';
 import 'applied_job_item.dart';
 
 class AppliedViewBody extends StatelessWidget {
-  const AppliedViewBody({
-    super.key,
-  });
+  const AppliedViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Padding(
         padding: const EdgeInsets.all(16),
-        child: Column(children: [AppliedJobItem()]),
+        child: Column(
+          children: List.generate(5, (index) {
+            return Padding(
+              padding: const EdgeInsets.only(bottom: 16),
+              child: AppliedJobItem(),
+            );
+          }),
+        ),
       ),
     );
   }
