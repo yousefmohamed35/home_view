00485eab74475db0261ceafed4ca1874529e0619
yousefmohamed35 <yousefmo2021jo@gmail.com>
2025-04-14 23:32:17 +0200

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:homeview/core/app_colors.dart';
 import 'package:homeview/core/service/service_locator.dart';
+import 'package:homeview/home/data/repos/home_repo_impl.dart';
+import 'package:homeview/home/presentation/manager/home_view_cubit.dart';
 
 import 'bottom_navigation_bar.dart';
 
@@ -15,11 +18,14 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: CustomBottomNavigationBar(),
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: AppColors.blue),
+    return BlocProvider(
+      create: (context) => HomeViewCubit(getIt.get<HomeRepoImpl>()),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: CustomBottomNavigationBar(),
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: AppColors.blue),
+        ),
       ),
     );
   }
