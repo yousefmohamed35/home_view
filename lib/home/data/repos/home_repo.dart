5c9ce9cafd0afb9abbfc4dd7fe674bd5bc4e4d9c
yousefmohamed35 <yousefmo2021jo@gmail.com>
2025-04-14 21:27:02 +0200

    create failure class and refactore home repo

--- lib/home/data/repos/home_repo.dart
+++ lib/home/data/repos/home_repo.dart
@@ -1,3 +1,7 @@
+import 'package:dartz/dartz.dart';
+import 'package:homeview/core/error/failure.dart';
+import 'package:homeview/home/data/models/home_model.dart';
+
 abstract class HomeRepo {
-  getAllJopPosts();
+ Future<Either<Failure,List<HomeModel>>> getAllJopPosts();
 }
