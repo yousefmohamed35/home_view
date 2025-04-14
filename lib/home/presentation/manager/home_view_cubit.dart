100df39118e3e96b4db5beb0ae1ad3fab15ae01b
yousefmohamed35 <yousefmo2021jo@gmail.com>
2025-04-14 23:08:33 +0200

    create get all job post method in home view cubit

--- lib/home/presentation/manager/home_view_cubit.dart
+++ lib/home/presentation/manager/home_view_cubit.dart
@@ -1,9 +1,23 @@
-import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:homeview/home/data/models/home_model.dart';
-import 'package:meta/meta.dart';
+import 'package:homeview/home/data/repos/home_repo.dart';
 
 part 'home_view_state.dart';
 
 class HomeViewCubit extends Cubit<HomeViewState> {
-  HomeViewCubit() : super(HomeViewInitial());
+  HomeViewCubit(this.homeRepo) : super(HomeViewInitial());
+  final HomeRepo homeRepo;
+  Future<void> getAllJobPosts() async {
+    emit(HomeViewLoading());
+    var result = await homeRepo.getAllJopPosts();
+    result.fold(
+      (fail) {
+        emit(HomeViewFailure(errorMessage: fail.errorMessage));
+      },
+      (jobs) {
+        emit(HomeViewSuccess(jobs: jobs));
+      },
+    );
+  }
 }
